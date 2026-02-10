b0062f6398331030c827b429ab92aa9bc3f9b949
Samuel Nye <samuelnyevr@gmail.com>
2026-02-09 18:24:54 -0800

    add step def

--- TestProject1/StepDefinitions/CalculatorStepDefinitions.cs
+++ TestProject1/StepDefinitions/CalculatorStepDefinitions.cs
@@ -1,28 +1,35 @@
-using System;
+using NUnit.Framework;
 using Reqnroll;
 
 namespace MyNamespace {
     [Binding]
-    public class StepDefinitions {
+    public class CalculatorStepDefinitions {
         private readonly ScenarioContext _scenarioContext;
 
-        public StepDefinitions(ScenarioContext scenarioContext) {
+        private decimal _firstNumber;
+        private decimal _secondNumber;
+        private decimal _result;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext) {
             _scenarioContext = scenarioContext;
         }
 
         [Given(@"I have entered (.*) into the calculator")]
-        public void GivenIhaveenteredintothecalculator(decimal args1) {
-            throw new PendingStepException();
+        public void GivenIHaveEnteredIntoTheCalculator(decimal number) {
+            if (_firstNumber == 0)
+                _firstNumber = number;
+            else
+                _secondNumber = number;
         }
 
         [When(@"I press add")]
-        public void WhenIpressadd() {
-            throw new PendingStepException();
+        public void WhenIPressAdd() {
+            _result = _firstNumber + _secondNumber;
         }
 
         [Then(@"the result should be (.*) on the screen")]
-        public void Thentheresultshouldbeonthescreen(decimal args1) {
-            throw new PendingStepException();
+        public void ThenTheResultShouldBeOnTheScreen(decimal expected) {
+            Assert.That(_result, Is.EqualTo(expected));
         }
     }
 }
