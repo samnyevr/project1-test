619508b54ea34e08975f57f48e1ead429efccb18
Samuel Nye <samuelnyevr@gmail.com>
2026-02-09 18:08:30 -0800

    fix

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,8 +1,5 @@
-﻿using NUnit.Framework;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using SeleniumExtras.WaitHelpers;
 
 namespace TestProject1;
 
